797314ac2eead1bb198226f1c7152dfd68930c81
Vladimir Jimenez <allejo@me.com>
2019-10-25 00:09:59 -0700

    Configure Jest coverage

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,7 @@
 module.exports = {
     preset: 'ts-jest',
+    collectCoverage: true,
+    coverageReporters: ['json', 'html'],
     testEnvironment: 'node',
     testPathIgnorePatterns: [
         '/node_modules/',
