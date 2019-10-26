f0b00cb733608be69eb9c607a523e1038d5c3a3c
Vladimir Jimenez <allejo@me.com>
2019-10-26 15:49:05 -0700

    Add unit test for SlashCommand block generation

---
+++ src/ChunkWriter/__tests__/SlashCommandChunk.test.ts
@@ -0,0 +1,37 @@
+import { ITestCodeDefinition, ITestCodeDefinitionRepeater } from '../../__tests__/utilities';
+import PluginBuilder from '../../PluginBuilder';
+import SlashCommandChunk from '../SlashCommandChunk';
+
+const tests: ITestCodeDefinition[] = [
+    {
+        desc: 'should output an event with data parameters in a switch statement',
+        setup: (def: PluginBuilder) => {
+            def.addSlashCommand({
+                name: 'killall'
+            });
+
+            def.addSlashCommand({
+                name: 'ban'
+            });
+        },
+        expected: `
+bool TestClass::SlashCommand(int playerID, bz_ApiString command, bz_ApiString /*message*/, bz_APIStringList *params)
+{
+    if (command == "killall")
+    {
+
+        return true;
+    }
+    else if (command == "ban")
+    {
+
+        return true;
+    }
+
+    return false;
+}
+        `,
+    },
+];
+
+ITestCodeDefinitionRepeater((c, d) => new SlashCommandChunk(c, d), tests);
