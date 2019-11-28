dbc67d012af7b0c009e2d77bf0cb0eba5378294c
Vladimir Jimenez <allejo@me.com>
2019-11-27 23:10:14 -0800

    Allow access to additional classes in PluginWriter

--- src/PluginWriter.ts
+++ src/PluginWriter.ts
@@ -1,6 +1,7 @@
 import { CPPClass, CPPFormatter, CPPVisibility } from 'aclovis';
 
 import CallbackChunk from './ChunkWriter/CallbackChunk';
+import { ChunkWriter } from './ChunkWriter/ChunkWriter';
 import CleanupChunk from './ChunkWriter/CleanupChunk';
 import EventChunk from './ChunkWriter/EventChunk';
 import IPlugin from './IPlugin';
@@ -10,28 +11,54 @@ import NameChunk from './ChunkWriter/NameChunk';
 import PollTypeChunk from './ChunkWriter/PollTypeChunk';
 import SlashCommandChunk from './ChunkWriter/SlashCommandChunk';
 
+/**
+ * @since 1.0.0
+ */
 export default class PluginWriter {
     private readonly pluginClass: CPPClass;
+    private readonly chunkWriters: ChunkWriter[];
 
-    constructor(private readonly plugin: IPlugin) {
+    public constructor(private readonly plugin: IPlugin) {
         this.pluginClass = new CPPClass(this.plugin.name);
         this.pluginClass.addExtends([CPPVisibility.Public, 'bz_Plugin']);
 
-        this.handleNameMethod();
-        this.handleInitMethod();
-        this.handleCleanupMethod();
-        this.handleCallbacks();
-        this.handleEvents();
-        this.handleSlashCommands();
-        this.handleMapObjects();
-        this.handlePollTypes();
+        this.chunkWriters = [
+            new NameChunk(this.pluginClass, this.plugin),
+            new InitChunk(this.pluginClass, this.plugin),
+            new CleanupChunk(this.pluginClass, this.plugin),
+            new CallbackChunk(this.pluginClass, this.plugin),
+            new EventChunk(this.pluginClass, this.plugin),
+            new SlashCommandChunk(this.pluginClass, this.plugin),
+            new MapObjectChunk(this.pluginClass, this.plugin),
+            new PollTypeChunk(this.pluginClass, this.plugin),
+        ];
+
+        this.chunkWriters.forEach((writer: ChunkWriter) => {
+            writer.process();
+        });
     }
 
-    getClassName(): string {
+    /**
+     * Get all of the additional classes that registered ChunkWriters may have
+     * created.
+     *
+     * @since 1.0.3
+     */
+    public getAdditionalClasses(): CPPClass[] {
+        return this.flatten(this.chunkWriters.map(writer => writer.getAdditionalClasses()));
+    }
+
+    /**
+     * @since 1.0.0
+     */
+    public getClassName(): string {
         return this.pluginClass.getClassName();
     }
 
-    getFormatter(): CPPFormatter {
+    /**
+     * @since 1.0.0
+     */
+    public getFormatter(): CPPFormatter {
         const { codeStyle } = this.plugin;
 
         return new CPPFormatter({
@@ -41,47 +68,25 @@ export default class PluginWriter {
         });
     }
 
-    write(): string {
+    /**
+     * @since 1.0.0
+     */
+    public write(): string {
         return this.pluginClass.write(this.getFormatter(), 0);
     }
 
-    private handleNameMethod() {
-        const nameWriter = new NameChunk(this.pluginClass, this.plugin);
-        nameWriter.process();
-    }
-
-    private handleInitMethod() {
-        const initWriter = new InitChunk(this.pluginClass, this.plugin);
-        initWriter.process();
-    }
-
-    private handleCleanupMethod() {
-        const cleanupWriter = new CleanupChunk(this.pluginClass, this.plugin);
-        cleanupWriter.process();
-    }
-
-    private handleEvents() {
-        const chunkWriter = new EventChunk(this.pluginClass, this.plugin);
-        chunkWriter.process();
-    }
-
-    private handleSlashCommands() {
-        const chunkWriter = new SlashCommandChunk(this.pluginClass, this.plugin);
-        chunkWriter.process();
-    }
-
-    private handleCallbacks() {
-        const callbackChunk = new CallbackChunk(this.pluginClass, this.plugin);
-        callbackChunk.process();
-    }
-
-    private handleMapObjects() {
-        const mapObjectChunk = new MapObjectChunk(this.pluginClass, this.plugin);
-        mapObjectChunk.process();
-    }
-
-    private handlePollTypes() {
-        const pollTypeChunk = new PollTypeChunk(this.pluginClass, this.plugin);
-        pollTypeChunk.process();
+    /**
+     * Flatten a multi-dimensional array into a one-dimensional array because I refuse to introduce `core-js` as a
+     * polyfill for `Array.prototype.flat()` simple because Node 10 LTS and IE/Edge don't support it.
+     *
+     * Seriously. Fuck JS.
+     *
+     * @param arr
+     *
+     * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/flat
+     * @see https://stackoverflow.com/a/15030117
+     */
+    private flatten(arr: Array<any>): Array<any> {
+        return arr.reduce((flat, next) => flat.concat(Array.isArray(next) ? this.flatten(next) : next), []);
     }
 }
--- src/__tests__/PluginWriter.test.ts
+++ src/__tests__/PluginWriter.test.ts
@@ -9,31 +9,55 @@ beforeEach(() => {
     pluginWriter = new PluginWriter(pluginDef.definition);
 });
 
-test('formatter set to 2 space indentation', () => {
-    pluginDef.definition.codeStyle.spacingType = 'twoSpace';
+describe('formatting settings', () => {
+    test('formatter set to 2 space indentation', () => {
+        pluginDef.definition.codeStyle.spacingType = 'twoSpace';
 
-    const fmtr = pluginWriter.getFormatter();
+        const fmtr = pluginWriter.getFormatter();
 
-    expect(fmtr.options.indentSpaceCount).toEqual(2);
-    expect(fmtr.options.indentWithSpaces).toEqual(true);
-    expect(fmtr.indentation).toEqual('  ');
+        expect(fmtr.options.indentSpaceCount).toEqual(2);
+        expect(fmtr.options.indentWithSpaces).toEqual(true);
+        expect(fmtr.indentation).toEqual('  ');
+    });
+
+    test('formatter set to 4 space indentation', () => {
+        pluginDef.definition.codeStyle.spacingType = 'fourSpace';
+
+        const fmtr = pluginWriter.getFormatter();
+
+        expect(fmtr.options.indentSpaceCount).toEqual(4);
+        expect(fmtr.options.indentWithSpaces).toEqual(true);
+        expect(fmtr.indentation).toEqual('    ');
+    });
+
+    test('formatter set to 4 space indentation', () => {
+        pluginDef.definition.codeStyle.spacingType = 'tabs';
+
+        const fmtr = pluginWriter.getFormatter();
+
+        expect(fmtr.options.indentWithSpaces).toEqual(false);
+        expect(fmtr.indentation).toEqual('\t');
+    });
 });
 
-test('formatter set to 4 space indentation', () => {
-    pluginDef.definition.codeStyle.spacingType = 'fourSpace';
+describe('ChunkWriters', () => {
+    describe('Additional classes', () => {
+        test('there should be no classes by default', () => {
+            expect(pluginWriter.getAdditionalClasses()).toHaveLength(0);
+        });
 
-    const fmtr = pluginWriter.getFormatter();
+        test('there should be one additional class', () => {
+            pluginDef.addMapObject({
+                uuid: '',
+                name: 'sample',
+                properties: [],
+            });
 
-    expect(fmtr.options.indentSpaceCount).toEqual(4);
-    expect(fmtr.options.indentWithSpaces).toEqual(true);
-    expect(fmtr.indentation).toEqual('    ');
-});
+            const writer = new PluginWriter(pluginDef.definition);
+            const additionalClasses = writer.getAdditionalClasses();
 
-test('formatter set to 4 space indentation', () => {
-    pluginDef.definition.codeStyle.spacingType = 'tabs';
-
-    const fmtr = pluginWriter.getFormatter();
-
-    expect(fmtr.options.indentWithSpaces).toEqual(false);
-    expect(fmtr.indentation).toEqual('\t');
+            expect(additionalClasses).toHaveLength(1);
+            expect(additionalClasses[0].getClassName()).toEqual('SampleZone');
+        });
+    });
 });
